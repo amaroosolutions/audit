868147b06e3f5276f973514797cec63985f87c38
Kiran Kantesariya <kirankantesariya@gmail.com>
2019-03-31 13:36:40 +0530

    [Dev] update method & Develop code

--- smile_audit/models/base.py
+++ smile_audit/models/base.py
@@ -14,15 +14,18 @@ class Base(models.AbstractModel):
     @api.model
     def create(self, vals):
         res = super(Base, self).create(vals)
+        print ("\n\n CREATE : ",res.display_name)
         if not res.display_name:
             res.display_name = res.name_get()[0][1]
         return res
 
     @api.multi
     def write(self, vals):
-        if vals.get('name'):
-            vals.update({'display_name': vals['name']})
-        return super(Base, self).write(vals)
+        res = super(Base, self).write(vals)
+        for rec in self:
+            if not rec.display_name:
+                rec.display_name = rec.name_get()[0][1]
+        return res
 
     @api.multi
     def _read_from_database(self, field_names, inherited_field_names=[]):
